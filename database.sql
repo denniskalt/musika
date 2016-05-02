36c8dfdfcefc9713e47bc863015a5e0528a426f5
Till Glöggler <till@gundk.it>
2016-05-02 10:13:29 +0200

    update database.sql

--- database.sql
+++ database.sql
@@ -1,6 +1,27 @@
-CREATE TABLE IF NOT EXISTS `user` (
+-- Adminer 4.2.3 MySQL dump
+
+SET NAMES utf8;
+SET time_zone = '+00:00';
+SET foreign_key_checks = 0;
+SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
+
+DROP TABLE IF EXISTS `album`;
+CREATE TABLE `album` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `username` varchar(255) NOT NULL,
-  `password` varchar(255) NOT NULL,
+  `artist` text NOT NULL,
+  `album` text NOT NULL,
+  `year` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-);
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(60) NOT NULL,
+  `password` varchar(60) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+-- 2016-05-02 07:59:57
